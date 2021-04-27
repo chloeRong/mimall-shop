256ffa3e776a04d1285937a529c1d0b760be8003
zourong <charice1988@163.com>
2021-04-27 16:58:11 +0800

    cross origin init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios'
+import jsonp from 'jsonp'
+
+export default {
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	mounted() {
+		const url = 'https://www.imooc.com/common/adver-getadver'
+		// axios.get(url).then((res) => {
+		// 	console.log(res)
+		// })
+		jsonp(url, (err, res) => {
+			console.log(res)
+		})
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
