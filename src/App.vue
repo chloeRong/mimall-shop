d7e4919679381c102193337aca9717ac6123c80a
zourong <charice1988@163.com>
2021-04-28 17:41:37 +0800

    路由配置

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 // import axios from 'axios'
 import jsonp from 'jsonp'
 
 export default {
 	name: 'App',
-	components: {
-		HelloWorld
-	},
+	components: {},
 	mounted() {
 		const url = '/api/common/adver-getadver'
 		// axios.get(url).then((res) => {
---
+++ src/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: 'home',
+	data() {
+		return {}
+	},
+	computed: {},
+	components: {},
+	created() {},
+	mounted() {},
+	methods: {}
+}
+</script>
+<style lang="stylus" scoped></style>
